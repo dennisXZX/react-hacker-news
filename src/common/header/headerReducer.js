edd800d7dd273a9a0d5108398d198aa178c5b43c
Dennis Xiao <dennisboys@gmail.com>
2019-01-11 19:48:49 +1000

    refactor reducers

--- src/common/header/headerReducer.js
+++ src/common/header/headerReducer.js
@@ -8,46 +8,71 @@ import {
 } from './headerActions'
 
 const defaultState = {
+  currentPage: 1,
+  currentSearch: '',
   isFocused: false,
   isHotSearchMouseIn: false,
   hotSearchList: [],
-  currentPage: 1,
   totalPage: 1
+}
+
+const _handleSearchFocus = (state, action) => {
+  return {
+    ...state,
+    isFocused: true
+  }
+}
+
+const _handleSearchBlur = (state, action) => {
+  return {
+    ...state,
+    isFocused: false
+  }
+}
+
+const _getHotSearchList = (state, action) => {
+  return {
+    ...state,
+    hotSearchList: action.hotSearchList,
+    totalPage: action.totalPage
+  }
+}
+
+const _handleHotSearchMouseEnter = (state, action) => {
+  return {
+    ...state,
+    isHotSearchMouseIn: true
+  }
+}
+
+const _handleHotSearchMouseLeave = (state, action) => {
+  return {
+    ...state,
+    isHotSearchMouseIn: false
+  }
+}
+
+const _changePage = (state, action) => {
+  return {
+    ...state,
+    currentPage: action.newCurrentPage
+  }
 }
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case SEARCH_FOCUS:
-      return {
-        ...state,
-        isFocused: true
-      }
+      return _handleSearchFocus(state, action)
     case SEARCH_BLUR:
-      return {
-        ...state,
-        isFocused: false
-      }
+      return _handleSearchBlur(state, action)
     case GET_HOT_SEARCH_LIST:
-      return {
-        ...state,
-        hotSearchList: action.hotSearchList,
-        totalPage: action.totalPage
-      }
+      return _getHotSearchList(state, action)
     case HOT_SEARCH_MOUSE_ENTER:
-      return {
-        ...state,
-        isHotSearchMouseIn: true
-      }
+      return _handleHotSearchMouseEnter(state, action)
     case HOT_SEARCH_MOUSE_LEAVE:
-      return {
-        ...state,
-        isHotSearchMouseIn: false
-      }
+      return _handleHotSearchMouseLeave(state, action)
     case CHANGE_PAGE:
-      return {
-        ...state,
-        currentPage: action.newCurrentPage
-      }
+      return _changePage(state, action)
     default:
       return state
   }
